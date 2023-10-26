b67907a0e7dd500425a70e95c358f4af1e7c8843
Pietro907 <pietrop.puri@gmail.com>
2023-10-26 10:18:01 +0200

    add fix style

--- index.php
+++ index.php
@@ -65,13 +65,19 @@ class Movie
     public $title;
     //variabile titolo originale
     public $originalTitle;
+    //variabile image
+    public $image;
+    //variabile image
+    public $trama;
 
     //Funzione __construct con il quale assegno un valore alla variabile dicendo 
     //di prendere il valore seguente ($this->title))
-    function __construct($title, $originalTitle)
+    function __construct($title, $originalTitle, $image, $trama)
     {
         $this->title = $title;
         $this->originalTitle = $originalTitle;
+        $this->image = $image;
+        $this->trama = $trama;
     }
     //Creo una funzione con multi echo
     /* function get_info(){
@@ -89,13 +95,26 @@ class Movie
     {
         return $this->originalTitle;
     }
+    //Creo un metodo per farmi ritonare il valore assegnato alla variabile
+    function get_image()
+    {
+        return $this->image;
+    }
+    //Creo un metodo per farmi ritonare il valore assegnato alla variabile
+    function get_trama()
+    {
+        return $this->trama;
+    }
 }
 
 class Strawberry extends Movie
 {
-    public function message()
+    public function card()
     {
-        echo "{$this->title} <--> Titolo? ";
+        echo "Titolo: <b>{$this->title}</b> <br>";
+        echo "<img src='{$this->image}' alt=''>";
+        echo "Titolo originale: {$this->originalTitle} <br>";
+        echo "Trama: {$this->trama} <br>";
     }
 }
 
@@ -103,19 +122,19 @@ class Strawberry extends Movie
 //Fuori dalla classe
 //Creo una variabile del singolo obj dal quale prendero le seguenti info
 // con l'aiuto dei metodi scritti in precedenza
-$film1 = new Movie("Titolo", "Original_Title");
+$film1 = new Movie("V per Vendetta",  "V for vendetta", "./assets/img/9780789315205-it.jpg", "lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum");
 
 //Ho utilizzato il nome Strawberry per dire un nome a caso XD
-$film2 = new Strawberry("Titolo", "Original_Title");
+$film2 = new Strawberry("V per Vendetta", "V for vendetta", "./assets/img/9780789315205-it.jpg",  "lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum");
 
 //Echo stampa a schermo
-echo ' <div class="container">
+/* echo ' <div class="container">
 <div class="row">
     <div class="col">
         <div class="card">
             <h3> <?php echo $film1->get_title() ?> </h3>
-            <h5>original_title</h5>
-            <p>vote</p>
+            <p> <?php echo $film1->get_original() ?> </p>
+            <h5> <?php echo $film2->message() ?> </h5>
         </div>
     </div>
     <div class="col">
@@ -134,16 +153,15 @@ echo '<br>';
 echo $film2->message();
 echo '<br>';
 echo '<br>';
-echo '<br>';
-
+echo '<br>'; */
 
 
 //Echo stampa a schermo
-echo $film2->get_title();
+/* echo $film2->get_title();
 echo '<br>';
 echo $film2->get_original();
 echo '<br>';
-echo $film2->message();
+echo $film2->message(); */
 
 ?>
 
@@ -157,28 +175,45 @@ echo $film2->message();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="./assets/css/style.css">
 </head>
 
 <body>
 
-<div class="container">
-<div class="row">
-    <div class="col">
-        <div class="card">
-            <h3><?php echo $film1->get_title() ?></h3>
-            <p><?php echo $film1->get_original() ?></p>
-            <h5><?php echo $film2->message() ?></h5>
+    <div class="container text-body-secondary mx-auto">
+        <div class="row my-5">
+
+            <div class="col">
+                <div class="card">
+
+                    <h5><?php echo 'Titolo: ' . '<br>' . $film1->get_title() ?></h5>
+                    <img src="<?php echo $film1->get_image() ?>" alt="">
+                    <p><?php echo 'Titolo originale: ' . $film1->get_original() ?></p>
+                    <p><?php echo 'Trama: ' . $film1->get_trama() ?></p>
+
+                </div>
+            </div>
+            
+            <div class="col">
+                <div class="card">
+
+                    <?php echo $film2->card(); ?>
+
+                </div>
+            </div>
+            
+            <div class="col">
+                <div class="card">
+
+                    <?php echo $film2->card(); ?>
+
+                </div>
+            </div>
+
+
+
         </div>
     </div>
-    <div class="col">
-        <div class="card">
-            <h3></h3>
-            <h5></h5>
-            <p></p>
-        </div>
-    </div>
-</div>
-</div>
 
 </body>
 
